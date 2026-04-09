ff989e7e79e8fd1239ea89c5265cbc8bda1eb888
SofianeTacherift <sofiane.t09@icloud.com>
2026-04-09 17:27:54 +0200

    ajout de declinaison fonctionel

--- controllers/admin_declinaison_chaussure.py
+++ controllers/admin_declinaison_chaussure.py
@@ -13,11 +13,46 @@ admin_declinaison_chaussure = Blueprint('admin_declinaison_chaussure', __name__,
 def add_declinaison_chaussure():
     id_chaussure=request.args.get('id_chaussure')
     mycursor = get_db().cursor()
-    chaussure=[]
-    couleurs=None
-    tailles=None
-    d_taille_uniq=None
-    d_couleur_uniq=None
+
+    sql=''' SELECT chaussure.id_chaussure,
+            chaussure.photo as image
+            FROM chaussure
+            WHERE chaussure.id_chaussure=%s'''
+    mycursor.execute(sql, (id_chaussure,))
+    chaussure=mycursor.fetchone()
+
+    sql=''' SELECT id_couleur,
+            libelle 
+            FROM couleur
+            '''
+    mycursor.execute(sql)
+    couleurs=mycursor.fetchall()
+
+    sql=''' SELECT id_taille,
+            libelle 
+            FROM taille
+        '''
+    mycursor.execute(sql)
+    tailles=mycursor.fetchall()
+
+    sql = '''   SELECT COUNT(*) as nb
+                FROM declinaison_chaussure
+                JOIN taille ON declinaison_chaussure.taille_id = taille.id_taille
+                WHERE declinaison_chaussure.chaussure_id = %s
+                AND taille.code_taille = 0'''
+    mycursor.execute(sql, (id_chaussure,))
+    d_taille_uniq = 1 if mycursor.fetchone()['nb'] > 0 else None
+
+
+    sql = '''   SELECT COUNT(*) as nb
+                FROM declinaison_chaussure
+                JOIN couleur ON declinaison_chaussure.couleur_id = couleur.id_couleur
+                WHERE declinaison_chaussure.chaussure_id = %s
+                AND couleur.code_couleur = 0'''
+    mycursor.execute(sql, (id_chaussure,))
+    d_couleur_uniq = 1 if mycursor.fetchone()['nb'] > 0 else None
+
+
     return render_template('admin/chaussure/add_declinaison_chaussure.html'
                            , chaussure=chaussure
                            , couleurs=couleurs
@@ -35,7 +70,27 @@ def valid_add_declinaison_chaussure():
     stock = request.form.get('stock')
     taille = request.form.get('taille')
     couleur = request.form.get('couleur')
-    # attention au doublon
+
+    sql=''' SELECT declinaison_chaussure.id_declinaison_chaussure
+            FROM declinaison_chaussure
+            WHERE declinaison_chaussure.taille_id=%s 
+            AND declinaison_chaussure.couleur_id=%s
+            AND declinaison_chaussure.chaussure_id=%s'''
+    mycursor.execute(sql,(taille,couleur,id_chaussure))
+    doublon=mycursor.fetchall()
+    if len(doublon)>=1:
+        sql=''' UPDATE declinaison_chaussure
+                SET declinaison_chaussure.stock=%s
+                WHERE declinaison_chaussure.couleur_id=%s 
+                AND declinaison_chaussure.taille_id=%s
+                AND declinaison_chaussure.chaussure_id=%s'''
+        mycursor.execute(sql, (stock,couleur,taille,id_chaussure))
+        flash('déclinaison déja existante, seul le stock de la déclinaison a été modifié', 'alert-warning')
+    else:
+        sql=''' INSERT INTO declinaison_chaussure (stock,chaussure_id,taille_id,couleur_id)
+                VALUES (%s,%s,%s,%s)'''
+        mycursor.execute(sql, (stock,id_chaussure,taille,couleur))
+
     get_db().commit()
     return redirect('/admin/chaussure/edit?id_chaussure=' + id_chaussure)
 
